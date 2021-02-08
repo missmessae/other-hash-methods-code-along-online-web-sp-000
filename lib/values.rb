310638cbb84620e5a36002c22e84f18965b2e7ce
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 00:11:26 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,6 +10,6 @@ require 'pry'
 
 def get_the_values(groceries)
   items = []
-  items << groceries[:dairy].values && groceries[:vegetable].values && groceries[:meat].values && groceries[:grains].values
+  items << groceries.values
   items
 end
